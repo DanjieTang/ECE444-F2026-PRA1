999020d5b3412c093c10bae9251bfeed9a765ade
Danjie Tang <danjie.tang@outlook.com>
2026-09-15 20:08:47 -0400

    Added unit test

--- functions.py
+++ functions.py
@@ -1,8 +1,14 @@
 class Utils:
+    @staticmethod
     def reversed(number: int) -> int:
+        if not isinstance(number, int):
+            return -1
         return int(str(number)[::-1])
 
-    def formatter(number: int) -> int:
+    @staticmethod
+    def formatter(number: int) -> tuple[str, str]:
+        if not isinstance(number, int):
+            return -1
         binary = bin(number)[2:]
         octal = oct(number)[2:]
         
---
+++ utils_test.py
@@ -0,0 +1,10 @@
+from functions import Utils
+
+# Input string
+Utils.reversed("string input")
+Utils.reversed(1.234) # Float input
+Utils.reversed(1234) # Int input
+
+Utils.formatter("string input")
+Utils.formatter(1.234) # Float input
+Utils.formatter(1234) # Int input
